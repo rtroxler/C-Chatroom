465523bc5c4f47d6aa5e36a41df46bc887bd7885
Ryan Troxler <rtroxler@ksu.edu>
2013-12-12 17:25:25 -0600

    Removed unused method.

--- client.c
+++ client.c
@@ -13,7 +13,6 @@
 #define PORT 2020 
 
 void clearprompt();
-char* concat(char *s1, char *s2);
 
 void setnonblocking(sock)
 int sock;
@@ -129,10 +128,3 @@ void clearprompt() {
     refresh();
 }
 
-char* concat(char *s1, char *s2)
-{
-    char *result = malloc(strlen(s1)+strlen(s2)+1);//+1 for the zero-terminator
-    strcpy(result, s1);
-    strcat(result, s2);
-    return result;
-}
